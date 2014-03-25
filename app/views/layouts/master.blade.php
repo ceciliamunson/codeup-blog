e2dd79bc472b661e86c87dbe388b7fff9c3a052f
ceciliamunson <ceci_ugarte@hotmail.com>
2014-03-25 11:11:01 -0500

    Updated links to use the action() method to reference the new controller methods

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -35,9 +35,9 @@
  		  <!-- Collect the nav links, forms, and other content for toggling -->
  		  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
  		    <ul class="nav navbar-nav">
- 		      <li><a href="/portfolio">Portfolio</a></li>
-              <li><a href="/resume">Resume</a></li>
-              <li><a href="/contactme">Contact Me</a></li>
+ 		      <li><a href="{{{ action('HomeController@showPortfolio') }}}">Portfolio</a></li>
+              <li><a href="{{{ action('HomeController@showResume') }}}">Resume</a></li>
+              <li><a href="{{{ action('HomeController@showContact') }}}">Contact Me</a></li>
  		      <li class="dropdown">
  		        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
  		        <ul class="dropdown-menu">
