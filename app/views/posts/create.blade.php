f33b0bb62cd186889b0d979ed9fd89e40a8d146d
ceciliamunson <ceci_ugarte@hotmail.com>
2014-03-25 15:24:00 -0500

    Changed form style and added left margin to h1

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -1,20 +1,28 @@
 @extends('layouts.master')
 
 @section('content')
-
-<h1>Create a New Post</h1>
-<form class="form-horizontal" role="form" method="POST" action="{{{ action('PostsController@store') }}}">
-	<div class="form-group">
-			<label for="title">Title</label>
-			<input id="title" name="title" type="text" value="{{{ Input::old('title') }}}">
-	</div>
-	<div class="form-group">
-			<label for="body">Body</label>
-			<textarea rows="6" name="body" id="body" >"{{{ Input::old('body') }}}"</textarea>
-	</div>
-	<div class="form-group">
-			<input type="submit" value="Create post">
-	</div>
-</form>
+<div class="container">
+	<h1 style="margin-left:120px;">Create a New Post</h1>
+	
+	<form class="form-horizontal" role="form" method="POST" action="{{{ action('PostsController@store') }}}">
+	  <div class="form-group">
+	    <label for="title" class="col-sm-2 control-label">Title</label>
+	    <div class="col-sm-10">
+	      <input type="text" class="form-control" id="title" name="title" value="{{{ Input::old('title') }}}">
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <label for="body" class="col-sm-2 control-label">Body</label>
+	    <div class="col-sm-10">
+	      <textarea rows="6" class="form-control" id="body" name="body">"{{{ Input::old('body') }}}"</textarea>
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <div class="col-sm-offset-2 col-sm-10">
+	      <button type="submit" class="btn btn-default">Create New Post</button>
+	    </div>
+	  </div>
+	</form>
+</div>
 	
 @stop
