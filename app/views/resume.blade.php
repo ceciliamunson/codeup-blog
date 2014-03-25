35e2ba7e4addf5c8f10b8d059a8c0277501b1a8b
ceciliamunson <ceci_ugarte@hotmail.com>
2014-03-25 09:37:20 -0500

    Created new view named resume.blade.php that extends from master.blade.php

---
+++ app/views/resume.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.master')
+
+
+@section('content')
+
+    <div class="container-fluid">
+		
+		<h1>CECILIA MUNSON <small>Junior Software Developer</small></h1>
+		<hr>
+		<div class="row">
+			<div class="col-md-2">CONTACT</div>
+			<div class="col-md-8">
+        <div class="row">
+          <div class="col-md-8">
+				    <p id="myname">Cecilia Munson</p>
+				    <p class="name"><a href="mailto:ceci_ugarte@hotmail.com">ceci_ugarte@hotmail.com</a></p>
+			    </div>
+       
+			    <div class="col-md-4">
+				    <p class="name"><a target="_blank" href="http://www.linkedin.com/in/ceciliamunson"><img class="logolg" src="/img/linkedin.jpeg"></a></p>
+				    <p class="name"><a  target="_blank" href="http://www.github.com/ceciliamunson"><img class="logosm" src="/img/github.jpeg"></a></p>
+			    </div>
+        </div>
+      </div>    
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-md-2">WORK EXPERIENCE</div>
+			<div class="col-md-8">
+				<div class="row">
+					<div class="col-md-8">
+						<p class="name"><a  target="_blank" href="http://www.chowtimevending.com">Chow Time Vending </a>
+							<span>San Antonio, TX</span></p>
+            <p>Owner/ Director of Operations</p>
+					</div>
+					<div class="col-md-4">
+            <p>2010 - 2014</p>
+          </div>
+				</div>
+        <div class="row">
+          <div class="col-md-8">
+            <br>
+            <p class="name"><a  target="_blank" href="http://www.seniorplc.com/">Senior Operations LLC </a>
+              <span>New Braunfels, TX</span></p>
+            <p>Account Manager</p>
+          </div>
+          <div class="col-md-4">
+            <br>
+            <p>2008 - 2010</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-8">
+            <br>
+            <p class="name"><a  target="_blank" href="http://www.ou.edu/content/police.html/">University of Oklahoma Police Department </a>
+              <span>Norman, OK</span></p>
+            <p>Community Service Office</p>
+          </div>
+          <div class="col-md-4">
+            <br>
+            <p>2008 - 2010</p>
+          </div>
+        </div>
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-md-2">EDUCATION</div>
+			<div class="col-md-8">
+        <div class="row">
+          <div class="col-md-8">
+				    <p class="name">LAMP+J Software Development Bootcamp</p>
+				    <p class="name"><a  target="_blank" href="http://www.codeup.com"><img class="logosm" src="img/codeup.png"></a><span>&nbsp;San Antonio, TX</span></p>
+          </div>
+          <div class="col-md-4">
+            <p>Graduating in Spring 2014</p>
+            <p style="width:80%" data-value="50">In Progress</p>
+            <progress max="100" value="50">
+              <div class="progress-bar">
+                <span style="width: 80%;">Progress: 80%</span>
+              </div>
+            </progress>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-8">
+            <br>
+            <p class="name">B.S. Industrial Engineering</p>
+            <p class="name"><a  target="_blank" href="http://www.ou.edu"><img class="logomd" src="img/ou.jpeg"></a><span>&nbsp;Norman, OK</span></p>
+            <p>Passed Fundamentals of Engineering Examination </p>
+            <p>Minor in German</p>
+          </div>
+          <div class="col-md-4">
+            <br>
+            <p>Graduated Fall 2007</p>
+          </div>
+        </div>
+      </div>
+		</div>
+	</div>
+
+@stop
