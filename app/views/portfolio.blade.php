4e199f2e73e32a8c73b0acc0e3baa365f80f8625
ceciliamunson <ceci_ugarte@hotmail.com>
2014-03-25 09:37:32 -0500

    Created new view named portfolio.blade.php that extends from master.blade.php

---
+++ app/views/portfolio.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container-fluid">
+		<h1>Portfolio <small>My work</small></h1>
+		<hr>
+		<div class="row">
+			<div class="col-md-6">
+				<a href="#" class="thumbnail">
+      				<img data-src="holder.js/700%x300" alt="...">
+    			</a>
+			</div>
+			<div class="col-md-6">
+				<p><h2>Project One</h2></p>
+				<p><h3>Subheading</h3></p>
+				<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+				<p>View Project > button</p>
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-md-2">
+				<a href="#" class="thumbnail">
+      				<img data-src="holder.js/60%x40" alt="...">
+    			</a>
+			</div>
+			<div class="col-md-10">
+				<p><h3>Media heading</h3></p>
+				<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2">
+				<a href="#" class="thumbnail">
+      				<img data-src="holder.js/60%x40" alt="...">
+    			</a>
+			</div>
+			<div class="col-md-10">
+				<p><h3>Media heading</h3></p>
+				<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2 col-md-offset-2">
+				<a href="#" class="thumbnail">
+      				<img data-src="holder.js/60%x40" alt="...">
+    			</a>
+			</div>
+			<div class="col-md-10">
+				<p><h3>Media heading</h3></p>
+				<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-md-4">
+				<a href="#" class="thumbnail">
+      				<img data-src="holder.js/300%x200" alt="...">
+    			</a>
+			</div>
+			<div class="col-md-4">
+				<a href="#" class="thumbnail">
+      				<img data-src="holder.js/300%x200" alt="...">
+    			</a>
+			</div>
+			<div class="col-md-4">
+				<a href="#" class="thumbnail">
+      				<img data-src="holder.js/300%x200" alt="...">
+    			</a>
+			</div>	
+		</div>
+@stop
