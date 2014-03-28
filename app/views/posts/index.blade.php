9401abe23287ba817d589389d9ea676e46526fce
ceciliamunson <ceci_ugarte@hotmail.com>
2014-03-28 11:44:05 -0500

    Added Str::words to show only a snippet of the post body

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -7,7 +7,7 @@
 		<a href="{{{ action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a>
 		<p class="form-group">{{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i A') }}}  by <a href="#">Cecilia Munson</a></p>
 	
-		<p>{{{ $post->body }}}</p>
+		<p>{{{ Str::words($post->body, 20) }}}</p>
 	</div>
 	
 @endforeach
