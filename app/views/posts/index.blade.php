b4e07a10234b52fa06b72b31c9630ecf4701bbf7
ceciliamunson <ceci_ugarte@hotmail.com>
2014-03-28 09:21:24 -0500

    Added update and edit buttons

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -3,14 +3,23 @@
 @section('content')
 
 @foreach ($posts as $post)
-<div class="blog-post">
-	<a href="{{{ action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a>
-	<p class="blog-post-meta">{{{ $post->created_at }}}<a href="#">Cecilia</a></p>
-
-	<p>{{{ $post->body }}}</p>
-</div>
+	<div class="blog-title">
+		<a href="{{{ action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a>
+		<p class="form-group">{{{ $post->created_at }}}<a href="#">Cecilia</a></p>
+	
+		<p>{{{ $post->body }}}</p>
+	</div>
+	<p>
+		<!-- <a href="{{{ action('PostsController@update') }}}">Update Post</a> -->
+	</p>
 @endforeach
+
 <p>
 	<a href="{{{ action('PostsController@create') }}}">Create New Post</a>
+</p>
+<p>
+	<a href="{{{ action('PostsController@edit', $post->id) }}}">Edit the Post</a>
+</p>
+
 
 @stop
