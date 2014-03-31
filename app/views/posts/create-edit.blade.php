dedbf864b1f247ac8002d2c53468b5f48d0e821a
ceciliamunson <ceci_ugarte@hotmail.com>
2014-03-31 08:59:15 -0500

    Added html

--- app/views/posts/create-edit.blade.php
+++ app/views/posts/create-edit.blade.php
@@ -16,35 +16,39 @@
 
 @endif
 
-
-	  <div class="form-group">
-	     {{ Form::label('title', 'Title', array('class' => 'col-sm-2 control-label')) }}
-	    <div class="col-sm-10">
-	      {{ Form::text('title', null, array('class' => 'form-control', 'placeholder' => 'Title')) }}
-	      {{ $errors->first('title', '<p><span class="help-block">:message</span></p>') }}
-	    </div>
-	  </div>
-	  <div class="form-group">
-	    {{ Form::label('body', 'Body', array('class' => 'col-sm-2 control-label')) }}
-	    <div class="col-sm-10">
-	      {{ Form::textarea('body', null, array('class' => 'form-control', 'row' => '5')) }}
-	      {{ $errors->first('body', '<p><span class="help-block">:message</span></p>') }}
-	    </div>
-	  </div>
-	  <div class="form-group">
-	    <div class="col-sm-offset-2 col-sm-10">
-	    	@if (empty($post->id))
-
-	      		<button type="submit" class="btn btn-default">Create New Post</button>
-
-	      	@else
-
-	      		<button type="submit" class="btn btn-default">Edit Post</button>
-
-	      	@endif
-	    </div>
-	  </div>
-	{{ Form::close() }}
+<div class="container">
+	<div class="blog-post-title">
+		<div class="form-group">
+		     {{ Form::label('title', 'Title', array('class' => 'col-sm-2 control-label')) }}
+		    <div class="col-sm-10">
+		      {{ Form::text('title', null, array('class' => 'form-control', 'placeholder' => 'Title')) }}
+		      {{ $errors->first('title', '<p><span class="help-block">:message</span></p>') }}
+		    </div>
+		</div>
+		<div class="form-group">
+		    {{ Form::label('body', 'Body', array('class' => 'col-sm-2 control-label')) }}
+		    <div class="col-sm-10">
+		      {{ Form::textarea('body', null, array('class' => 'form-control', 'row' => '5')) }}
+		      {{ $errors->first('body', '<p><span class="help-block">:message</span></p>') }}
+		    </div>
+		</div>
+		<div class="form-group">
+		    <div class="col-sm-offset-2 col-sm-10">
+		    	@if (empty($post->id))
+	
+		      		<button type="submit" class="btn btn-default">Create New Post</button>
+	
+		      	@else
+	
+		      		<button type="submit" class="btn btn-default">Edit Post</button>
+	
+		      	@endif
+		    </div>
+		</div>
+	</div>
+</div>
+	
+{{ Form::close() }}
 
 	
 @stop
