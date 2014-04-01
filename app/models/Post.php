e238cfa7d9ae9ada7334b18b22fa2b88705204a2
ceciliamunson <ceci_ugarte@hotmail.com>
2014-04-01 13:18:14 -0500

    Created a one-to-many relationship between users and their posts

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,6 +4,13 @@ class Post extends BaseModel {
 
     protected $table = 'posts';
 
+    // Define relationship to user (author)
+    public function user()
+    {
+    	return $this->belongsTo('User');
+    }
+
+    // Validation rules
     public static $rules = array(
 	    'title'      => 'required|max:100',
 	    'body'       => 'required|max:10000'
